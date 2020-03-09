65ffff3907038ab963de7ef7e1be2288c2a0ddaf
Rogelio97 <34067619+Rogelio97@users.noreply.github.com>
2020-03-09 17:07:25 -0600

    Add files via upload

---
+++ Ejercicio#1.js
@@ -0,0 +1,27 @@
+//Ejercicio#1
+//Declaracion de vaiables
+console.log("Ejercicio #1")
+console.log("Determina el resultado de un número x elevado a una potencia n.")
+
+const potencia = (a,b) =>{
+	let result = Math.pow(a,b)
+	return result;
+};
+console.log(potencia(3,4));
+
+
+
+
+
+
+
+
+
+
+
+//de lo general a lo particular, tecnologias en general. redactado en tercera persona, 
+//segundo parrafo empresas que desaroollan videojuegos
+//tercer parrafo hablar del videjuego, como esta conformado,
+//antecedentes mencionar empresas que hacen referencias del videojuego que tengan diferentes 
+
+//marco teorico, marco coneceptual, marco referencial y marco historico 
